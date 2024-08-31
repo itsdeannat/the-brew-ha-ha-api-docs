d058010981960a995e44d532a8cb061c4994a1f2
Deanna Thompson <deanna.thompson45@outlook.com>
2024-08-31 11:17:14 -0400

    change: Change website homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Brew API Docs',
+			title: 'Brew Ha Ha API Docs',
 			social: {
 				github: 'https://github.com/itsdeannat/the-brew-ha-ha-api-docs.git',
 			},
