c29bac8caf89ae4536dcccb56d1b57915c8e1d3b
Deanna Thompson <deanna.thompson45@outlook.com>
2024-11-13 18:12:31 -0500

    fix: Fix bugs in snippets

--- src/snippets/get_requests/get_request_id_response.sh
+++ src/snippets/get_requests/get_request_id_response.sh
@@ -1,9 +1,9 @@
 {
-    "id": "3",
-    "coffee_type": "cortado",
+    "id": 3,
+    "product_name": "cortado",
     "temperature": "hot",
     "caffeine_amount": 130,
     "price": 4.0,
     "description": "Made with beans picked from the coast of Spain",
-    "in_stock": True
+    "quantity": 5
 }
